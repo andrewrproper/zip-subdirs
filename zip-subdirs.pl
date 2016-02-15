d67a488f39735db5d38e1c780401be5a57c2ca62
Andrew Proper <andrewrproper@gmail.com>
2016-02-15 14:04:46 -0500

    move copyright and license to top
    modified:   zip-subdirs.pl

--- zip-subdirs.pl
+++ zip-subdirs.pl
@@ -9,6 +9,36 @@ BEGIN {
 	use File::Spec 3.33;
 	use FindBin; 
 }
+
+
+=head1 Author
+
+Created by Andrew Proper, 2016
+
+https://endosynth.wordpress.com/
+
+andrewrproper@gmail.com
+
+
+=head1 License : GPL v3
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+
+
 use strict;
 use warnings;
 use feature ':5.10';
@@ -466,34 +496,6 @@ my $app = MyApp->new;
 # will not return until the last frame is closed
 
 $app->MainLoop;
-
-__END__
-
-=head1 Author
-
-Created by Andrew Proper, 2016
-
-https://endosynth.wordpress.com/
-
-andrewrproper@gmail.com
-
-
-=head1 License : GPL v3
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-=cut
 
 
 
